0e3328c20ea5fed356f5fb12f6e83dd45c1d40d4
ibula <bulaivan@mx1.ibm.com>
2013-08-31 01:45:46 -0500

    mod default recipe

--- cookbooks/rails-demo/metadata.rb
+++ cookbooks/rails-demo/metadata.rb
@@ -1,9 +1,9 @@
-name             'rails-demo'
-maintainer       'sccd'
-maintainer_email 'bulaivan@mx1.ibm.com'
-license          'All rights reserved'
-description      'Installs/Configures rails-demo'
+name             "rails-demo"
+maintainer       "sccd"
+maintainer_email "bulaivan@mx1.ibm.com"
+license          "All rights reserved"
+description      "Installs/Configures rails-demo"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-depends          'application'
-depends          'application_ruby'
+version          "0.1.0"
+depends          "2application"
+depends          "application_ruby"
